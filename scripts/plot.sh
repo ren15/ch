7d9d9af525e91634fdcf2f37f9d2f62c794ffe08
Ren Wang <iamwrm@gmail.com>
2022-08-16 06:24:48 +0000

    70

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -1,11 +1,9 @@
-set -x
+set -e
 
 eval "$(${HOME}/miniconda3/bin/conda shell.bash hook)"
 
-
-set -e
+set -x
 
 python src/create_image_from_csv.py
-
 
 
--- scripts/test.sh
+++ scripts/test.sh
@@ -42,7 +42,7 @@ clickhouse-client --query="${query}" > data/trips.parquet
 ls data/
 du -d1 -h data
 
-python src/create_dump.py 20
+python src/create_dump.py 70
 
 ls data/
 du -d1 -h data
